30f3645a5271f9aa605cf5dc8fe28a4ee560da83
Hafeez <syedhafeez957@gmail.com>
2026-01-15 23:33:07 +0530

    added discovery-server

---
+++ discovery-server/src/test/java/com/workergo/discovery_server/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workergo.discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
