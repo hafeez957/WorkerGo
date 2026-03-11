a0b11e14c6bd4807efe0af24ff6608935faf8e17
Hafeez <syedhafeez957@gmail.com>
2026-03-11 22:24:19 +0530

    Connected with frontend

--- auth-service/src/main/java/com/workergo/auth_service/config/SecurityConfig.java
+++ auth-service/src/main/java/com/workergo/auth_service/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.workergo.auth_service;
+package com.workergo.auth_service.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
